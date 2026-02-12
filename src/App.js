6a4a905d80309130f6d8d193569270f48c430c05
sidiki99 <msidiki075@gmail.com>
2026-02-12 11:04:59 +0500

    color add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { useState, useEffect } from "react";
 
 const initialFriends = [
-  {
-    id: 118836,
-    name: "Haris Sajid",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-    whatsapp: "923023976388",
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-    whatsapp: "923020730970",
-  },
+  // {
+  //   id: 118836,
+  //   name: "Haris Sajid",
+  //   image: "https://i.pravatar.cc/48?u=118836",
+  //   balance: -7,
+  //   whatsapp: "923052233938",
+  // },
+  // {
+  //   id: 933372,
+  //   name: "Sarah",
+  //   image: "https://i.pravatar.cc/48?u=933372",
+  //   balance: 20,
+  //   whatsapp: "923020730970",
+  // },
   {
     id: 499476,
-    name: "Aqib",
+    name: "Mudassir Siddiqui",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
-    whatsapp: "923020730970",
+    whatsapp: "923277379390",
   },
 ];
 
